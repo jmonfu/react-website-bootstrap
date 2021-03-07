4fb69305ee617ca3678055c229527292a254a4c6
Johann Montfort <jmonfu@gmail.com>
2021-03-07 20:14:33 +0100

    Cards

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../App.css";
+import Cards from "../Cards";
 import HeroSection from "./../HeroSection";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 };
